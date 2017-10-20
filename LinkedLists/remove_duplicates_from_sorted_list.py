27632acc0acd83edb91252bcc167783fd7122c84
Justin Zwick <zwickyj@gmail.com>
2017-10-20 01:01:10 -0400

    improved explanation/comments

--- LinkedLists/remove_duplicates_from_sorted_list.py
+++ LinkedLists/remove_duplicates_from_sorted_list.py
@@ -12,8 +12,14 @@ class Solution(object):
         """
         curr = head
         while curr and curr.next:
+            # Because the input list is sorted,
+            # you can determine if a node is a duplicate 
+            # by comparing its value to the node after it in the list. 
             if curr.val == curr.next.val:
+                # if it is, "skip" the next node
+                # by changing the next pointer
                 curr.next = curr.next.next
             else:
+                # otherwise, keep traversing the linked list
                 curr = curr.next
         return head
