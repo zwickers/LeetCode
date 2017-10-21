eb5fa0f598426d85506e8ba9420f918f4a58bcfe
Justin Zwick <zwickyj@gmail.com>
2017-10-21 18:05:10 -0400

    Create two_sum.py

---
+++ HashTables/two_sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # KEYS: numbers from the nums list
+        # VALUES: the respective indices of the numbers from the nums list
+        buff_dict = {}
+        
+        # iterate through the nums list
+        for i in range(len(nums)):
+            # check if each element's complement (target - nums[i]) exists in the dict
+            if (target - nums[i]) in buff_dict:
+                # if it does, return the index of the current list element
+                # and the index of it's complement
+                return [i,buff_dict.get(target - nums[i])]
+            # add each element's value and its index to the dict
+            else:
+                buff_dict[nums[i]] = i
