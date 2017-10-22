ed50617c0bf37a83abaaabf054582eef619a3946
Justin Zwick <zwickyj@gmail.com>
2017-10-22 17:10:05 -0400

    improved explanation for max_depth.py

--- Trees/max_depth.py
+++ Trees/max_depth.py
@@ -11,7 +11,10 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        # base case: if current node is None, return 0
         if not root:
             return 0
         
+        # recursive case: if current node does exist, add 1 to the
+        # max() of the left and right subtrees
         return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
