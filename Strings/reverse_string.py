f557f40fe6ee032817eddf9587d1dbcb400a3f5a
Justin Zwick <zwickyj@gmail.com>
2017-11-08 13:45:27 -0500

    Create reverse_string.py

---
+++ Strings/reverse_string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        # this trivial problem can be solved
+        # using basic slicing
+        
+        return s[::-1]
