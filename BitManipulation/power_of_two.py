d26d5b05934a2e77876fbcfa74bc4cc5a5fa02b2
Justin Zwick <zwickyj@gmail.com>
2018-01-14 22:37:52 -0500

    optimized previous solution

--- BitManipulation/power_of_two.py
+++ BitManipulation/power_of_two.py
@@ -4,15 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """       
-        # edge case: n == 1, in this case there are no zero's at all because
-        # of the way python stores binary numbers
-        # '0b1'
-        if n == 1:
-            return True
         
-        # if the MSB is a 1 and all other bits
-        # are 0s, then it is a power of 2
-        if bin(n)[2] == '1' and bin(n)[3:].count('1') == 0:
+        if n <= 0:
+            return False
+        
+        # n & n-1 returns the value of n with the lowest set bit erased
+        if (n & (n-1)) == 0:
             return True
         else:
             return False
+        
