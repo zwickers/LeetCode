0c54f9917ccc3b90ed100e18c83df2d7217bba4c
Justin Zwick <zwickyj@gmail.com>
2018-01-15 20:50:08 -0500

    Update is_power_of_four.py

--- BitManipulation/is_power_of_four.py
+++ BitManipulation/is_power_of_four.py
@@ -4,16 +4,7 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        # PATTERN:
-        # 4^0 = 1  = 1
-        # 4^1 = 4  = 100
-        # 4^2 = 16 = 10000
-        # 4^3 = 64 = 1000000
-        # i.e., MSB is 1, all other bits are 0, and there are an odd number of bits
-        
-        if num == 1:
-            return True
-        if bin(num)[2] == '1' and bin(num)[3:].count('1') == 0 and len(bin(num)[2:]) % 2 != 0:
-            return True
-        else:
-            return False
+       
+        # 0x5 is 0101
+        return (num > 0) and num & ((num - 1) == 0) and (num & 0x55555555 == num)
+    
