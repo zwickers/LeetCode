f9358420ca81ade58821ba1e57c489c426614702
Justin Zwick <zwickyj@gmail.com>
2018-08-04 14:46:39 -0700

    Create AverageLevels.java

---
+++ Trees/AverageLevels.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // this is just BFS, with a twist
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        double sum = 0.0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+                sum += curr.val;
+            }
+            sum = sum / size;
+            result.add(sum);
+            sum=0;
+        }
+        
+        return result;
+    }
+}
